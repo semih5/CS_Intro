fbfe6f9fe591561518a9337c2b4028b9796feb39
Muhammed Semih Kundakcı <semihkundakci5@gmail.com>
2021-01-20 20:34:03 +0300

    s

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Semih";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "c#";
+            kurs1.KursEgitmeni = "Engin";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.KursEgitmeni = "Kerem";
+            kurs2.IzlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.KursEgitmeni = "Berkay";
+            kurs3.IzlenmeOrani = 80;
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.KursEgitmeni);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+
+        public string KursEgitmeni { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
+
