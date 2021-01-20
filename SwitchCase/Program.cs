11cacd5ac42051e9a19dce7e33a353f7464c3f00
Muhammed Semih Kundakcı <semihkundakci5@gmail.com>
2021-01-20 23:10:59 +0300

    SwitchCase

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert day you are on;");
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Today is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Today is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Today is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Today is Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Today is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Today is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday");
+                    break;
+
+
+            }
+
+            //Using the computer's date to find a day we are on.
+            int day = (int)DateTime.Now.DayOfWeek;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Today is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Today is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Today is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Today is Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Today is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Today is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday");
+                    break;
+            }
+
+            Console.WriteLine("Please insert month you are in;");
+            int month = Convert.ToInt32(Console.ReadLine());
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Winter");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Autumn");
+                    break;
+
+            }
+
+        }
+    }
+}
+
+  
