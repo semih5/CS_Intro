3b949fa4f790b8e5398014904166015e2d3cb67f
Muhammed Semih Kundakcı <semihkundakci5@gmail.com>
2021-01-20 20:38:51 +0300

    loops

---
+++ Loops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //böyle tek tek yazılmaz
+            string kurs1 = "yazılım geliştirici kursu";
+            string kurs2 = "programlamaya giriş";
+
+            //array-diziler kullanılır
+
+            string[] kurslar = new string[] { "yazılım geliştirici kursu",
+            "programlamaya giriş", "java"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+
+            }
+            Console.WriteLine("for bitti");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu");
+
+            //for (int i = 1; i < 10; i=i+2) //i+=2 şeklinde de yazılabilir
+            //{
+            //  Console.WriteLine(i);
+            //}
+        }
+    }
+}
