593896c92cf7a48daab66ed6e6c925745e68030d
Muhammed Semih Kundakcı <semihkundakci5@gmail.com>
2021-01-21 23:21:49 +0300

    20.01

---
+++ Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Names = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(Names[0]);
+            Console.WriteLine(Names[1]);
+            Console.WriteLine(Names[2]);
+            Console.WriteLine(Names[3]);
+            Names.Add("İlker");
+            Console.WriteLine(Names[4]);
+            Console.WriteLine(Names[1]);
+
+        }
+    }
+}
